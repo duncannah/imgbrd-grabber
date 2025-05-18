b0aa46418600f635ce5b96191744bd132a5a5912
duncannah <38371914+duncannah@users.noreply.github.com>
2025-05-18 18:45:45 +0200

    test
    skip-checks: true

--- scripts/package-appimage.sh
+++ scripts/package-appimage.sh
@@ -12,11 +12,19 @@ pushd build
     make DESTDIR=../$APP_DIR install
 popd
 
-# See https://github.com/probonopd/linuxdeployqt
-export VERSION=$GRABBER_VERSION
-./vendor/linuxdeployqt.AppImage "$APP_DIR/usr/share/applications/org.bionus.Grabber.desktop" -appimage -extra-plugins=platforms/libqoffscreen.so -updateinformation="gh-releases-zsync|Bionus|imgbrd-grabber|latest|Grabber_*-arm64.AppImage.zsync"
-mv "Grabber-$VERSION-arm64.AppImage" "Grabber_$VERSION-arm64.AppImage"
-mv "Grabber-$VERSION-arm64.AppImage.zsync" "Grabber_$VERSION-arm64.AppImage.zsync"
+# Download and make appimagetool executable
+wget -c https://github.com/probonopd/go-appimage/releases/download/continuous/appimagetool-886-aarch64.AppImage
+chmod +x appimagetool-886-aarch64.AppImage
+
+# Deploy dependencies and create AppImage
+./appimagetool-886-aarch64.AppImage -s deploy "$APP_DIR/usr/share/applications/org.bionus.Grabber.desktop"
+./appimagetool-886-aarch64.AppImage "$APP_DIR"
+
+# Rename the AppImage
+mv Grabber-aarch64.AppImage "Grabber_${VERSION}-arm64.AppImage"
+
+# Optional: Generate .zsync file for updates
+./appimagetool-886-aarch64.AppImage -u "gh-releases-zsync|Bionus|imgbrd-grabber|latest|Grabber_*-arm64.AppImage.zsync" "$APP_DIR"
 
 # Cleanup
 rm -rf $APP_DIR
